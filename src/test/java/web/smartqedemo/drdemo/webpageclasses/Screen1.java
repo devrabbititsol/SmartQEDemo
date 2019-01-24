7c892c8082da18f397bb6234dcf3bed9c860a956
santhosh-annam <santhosh.annam@devrabbit.com>
2019-01-24 12:12:33 +0530

    first commit

--- src/test/java/web/smartqedemo/drdemo/webpageclasses/Screen1.java
+++ src/test/java/web/smartqedemo/drdemo/webpageclasses/Screen1.java
@@ -9,7 +9,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 @SuppressWarnings("unused")
 public class Screen1 extends BaseClass {
 	
-	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://devrabbitdev.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"SmartQEDemo\",\"project_description\":\"desc\",\"project_id\":199,\"module_name\":\"DRDemo\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":343,\"testcase_name\":\"TC2\",\"testcase_id\":407,\"testset_id\":0,\"executed_timestamp\":2060001806,\"browser_type\":\"chrome\"}";
+	public static String primaryInfo  = "{\"user_id\":7,\"is_generate\":false,\"is_execute\":false,\"is_web\":true,\"project_url\":\"https://devrabbitdev.com/\",\"report_upload_url\":\"https://192.168.1.142:8080/TAF_Automation_DR/UploadReportFile\",\"project_name\":\"SmartQEDemo\",\"project_description\":\"desc\",\"project_id\":199,\"module_name\":\"DRDemo\",\"module_description\":\"desc\",\"sub_module_id\":0,\"module_id\":343,\"testcase_name\":\"TC1\",\"testcase_id\":406,\"testset_id\":0,\"executed_timestamp\":2123925018,\"browser_type\":\"chrome\"}";
 
 	public static String projectName = "smartqedemo";
 	public WebDriver driver;
@@ -23,10 +23,11 @@ public class Screen1 extends BaseClass {
 
 	@FindBy(how = How.XPATH, using = "//HTML/BODY[1]/DIV[1]/DIV[1]/DIV[1]/DIV[1]/UL[1]/LI[2]/A[1]")	
 	private WebElement	Login_290526A;
-	public void clkALogin_290526() {
+	public String clkALogin_290526() {
 		waitForExpectedElement(driver, Login_290526A);		
 		String text = Login_290526A.getText();
 		Login_290526A.click();
+		return text;
 	}
 
 }
